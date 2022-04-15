28cecaa080dd65ca611958814dc3eb87c5403138
Alexander Wyatt <alex.wyatt20@gmail.com>
2022-04-15 15:18:17 -0400

    Updated main and fixing patient data retrieval

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -103,7 +103,7 @@ public class Main {
     //System.out.println(patientApps.getPatient_ID());
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-       ResultSet rs = stmt.executeQuery("SELECT * FROM appointment WHERE appointment.patient_id = '" + patID + "';");
+       ResultSet rs = stmt.executeQuery("SELECT * FROM appointment WHERE appointment.patient_id = '" + patID + "'");
       //ResultSet rs = stmt.executeQuery("SELECT * FROM patient WHERE patient.patient_id = " + patID);
 
       if (isRSEmpty(rs)) {
